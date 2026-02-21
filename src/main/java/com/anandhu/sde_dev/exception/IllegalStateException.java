db385eac34720035a646bb203aab6cf21ac20e87
anandhu <anandhuzz916@gmail.com>
2026-02-21 21:09:52 +0530

    Add frontend dependencies, initial styling, and backend compiled classes.

--- src/main/java/com/anandhu/sde_dev/exception/IllegalStateException.java
+++
@@ -1,7 +0,0 @@
-package com.anandhu.sde_dev.exception;
-
-public class IllegalStateException extends RuntimeException{
-    public IllegalStateException(String message) {
-        super(message);
-    }
-}
